abb23b2aaabf99f88388a6a5ee6950aaf2fde704
Simon Rofe <simon.rofe@ieee.org>
2022-12-12 07:40:22 +1000

    Add initial test an implementation

---
+++ MereMemo/Log.h
@@ -0,0 +1,15 @@
+#ifndef MEREMEMO_LOG_H
+#define MEREMEMO_LOG_H
+
+#include <fstream>
+#include <iostream>
+#include <string_view>
+
+namespace MereMemo {
+    inline void log(std::string_view message) {
+        std::fstream logFile("application.log", std::ios::app);
+        logFile << message << std::endl;
+    }
+}
+
+#endif //MEREMEMO_LOG_H
---
+++ tests/Construction.cc
@@ -0,0 +1,6 @@
+#include "../MereMemo/Log.h"
+#include "lib/MereTDD.h"
+
+TEST("Simple message can be logged") {
+    MereMemo::log("simple");
+}
