9e5996af697d0619f737021c52ca6d357876368d
Simon Rofe <simon.rofe@ieee.org>
2022-12-12 11:45:45 +1000

    Add a more complicated message

--- tests/Construction.cc
+++ tests/Construction.cc
@@ -9,3 +9,11 @@ TEST("Simple message can be logged") {
     bool result = Util::isTextInFile(message, "application.log");
     CONFIRM_TRUE(result);
 }
+
+TEST("Complicated message can be logged") {
+    std::string message = "complicated ";
+    message += Util::randomString();
+    MereMemo::log() << message << " double=" << 3.14 << " quoted=" << std::quoted("in quotes");
+    bool result = Util::isTextInFile(message, "application.log");
+    CONFIRM_TRUE(result);
+}
